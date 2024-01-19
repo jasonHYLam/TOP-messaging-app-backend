14f3acb3d15d17dd18db7c740b37b6acc32b1ae2
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-01-19 21:24:45 +0000

    Require Controller Files In Index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+const chatController = require('../controllers/chatController');
+const loginController = require('../controllers/loginController');
+const messageController = require('../controllers/messageController');
+const personalProfileController = require('../controllers/personalProfileController');
+const userController = require('../controllers/userController');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+
 
 module.exports = router;
