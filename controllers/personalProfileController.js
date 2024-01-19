3707db30685d116086215ed413f6c9ebae3a129e
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-01-19 21:22:14 +0000

    Add Change_Image To personalProfileController

--- controllers/personalProfileController.js
+++ controllers/personalProfileController.js
@@ -5,10 +5,19 @@ const he = require('he');
 
 const User = require('../models/user');
 
+// Will not bother with changing username and password.
+// Regarding changing username, not sure how to deal with that logic.
+
 exports.change_description = [
     // body(),
     asyncHandler(async (req, res, next) => {
 
     })
+] 
 
+exports.change_image = [
+    // body(),
+    asyncHandler(async (req, res, next) => {
+
+    })
 ] 
