c3e622a6b8080aefb23163a8131f3472d23400dc
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-01-19 20:23:25 +0000

    Create MessageController File
    This is used to handle message model related functions. An empty
    `create_message` callback function was also created in this commit.

---
+++ controllers/messageController.js
@@ -0,0 +1,12 @@
+const asyncHandler = require("express-async-handler");
+
+exports.create_message = [
+    // validate the text first
+    // body().trim().escape(),
+
+    asyncHandler( async(req, res, next) => {
+
+        // create Message model
+    })
+
+]
