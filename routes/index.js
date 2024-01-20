8264dc009cf5dc9b0ad4dfac9e119cd91bf1410f
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-01-20 13:19:54 +0000

    Add Login And Chat Related Routes To Index.js

--- routes/index.js
+++ routes/index.js
@@ -13,8 +13,21 @@ router.get('/', function(req, res, next) {
 });
 
 // hm is this right? multiple URLs... I guess so!
-// will i need to protect all routes...? damn
-router.get('/home/chat:chatid', chatController.get_chat_messages);
+// will i need to protect all routes (except login/signup/logout)...? damn
+
+// Login related routes.
+router.post('/signup', loginController.signup);
+router.post('/login', loginController.login);
+router.post('/logout', loginController.logout);
+
+// Chat related routes.
+// Get specific chat's messages.
+router.get('/home/chat/:chatid', chatController.get_chat_messages);
+// Create new chat. Need to figure out what the route should be called.
+router.post('/home/create_new_chat', chatController.create_new_chat);
+
+// Message related routes.
+// router.post('/home/chat/:chatid/create_message', )
 
 
 module.exports = router;
