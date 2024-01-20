6cd773f84c805251e86c4d505240eb131ee7293d
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-01-20 14:39:02 +0000

    Add Mongo URI To MongoConfig

--- mongoConfig.js
+++ mongoConfig.js
@@ -1,3 +1,10 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 
-const mongoDB = `${}`;
+const mongoDB = `${process.env.MONGODB_URI}`;
+
+mongoose.connect(mongoDB);
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'mongo connection error'));
