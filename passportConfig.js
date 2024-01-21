2d77f60778721aee0db960c19fd5e65bd618380a
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-01-21 12:30:41 +0000

    Make LocalStrategy Verify Callback Function

--- passportConfig.js
+++ passportConfig.js
@@ -1,2 +1,27 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
+
+const User = require('./models/user');
+
+// when is this called?
+passport.use(
+    new LocalStrategy( async (username, password, done) => {
+        try {
+            const user = await User.findOne({ username: username })
+            if (!user) {
+                // what happens after this
+                return done(null, false, { message: 'Incorrect username '})
+            }
+            if (user.password !== password) {
+                return done(null, false, { message: 'Incorrect password'})
+            }
+
+            return done(null, user);
+        }
+
+        catch(err) {
+            return done(err)
+        }
+
+    })
+)
