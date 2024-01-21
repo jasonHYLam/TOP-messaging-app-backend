6cf1f6092ff7d330bd336766f266e52f4bcc1ee0
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-01-21 12:36:27 +0000

    Make SerialiseUser And DeserializeUser Functions

--- passportConfig.js
+++ passportConfig.js
@@ -25,3 +25,16 @@ passport.use(
 
     })
 )
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+})
+
+passport.deserializeUser(async (id, done) => {
+    try {
+        const user = await User.findById(id)
+        done(null, user)
+    } catch(err) {
+        done(err)
+    } 
+})
