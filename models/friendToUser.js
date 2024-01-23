42a85ee544499c6e8e276f9dd5a501242da158aa
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-01-23 21:09:23 +0000

    Create FriendToUser Model
    This should make it easy to get friends of a user.

---
+++ models/friendToUser.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const FriendToUserSchema = new Schema({
+    user: { type: Schema.Types.ObjectId, ref: "User"},
+    friendUser: { type: Schema.Types.ObjectId, ref: "User"},
+})
+
+module.exports = mongoose.model('FriendToUserSchema', FriendToUserSchema);
+
