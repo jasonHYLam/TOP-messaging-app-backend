48a97554e2d2aa2c8380aaac09723fafe14b0109
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-02-11 23:44:54 +0000

    Create Users Array For Testing
    This is an array of objects representing users, to be used to create
    User documents and for comparing values.

--- tests/testConfig/users.js
+++ tests/testConfig/users.js
@@ -1,5 +1,39 @@
+const { ObjectId } = require('mongoose').Types;
 const users = [
     {
-        
-    }
+        _id: ObjectId(`65be5813dad7a9ce85209436`),
+        username: 'user1',
+        password: '$2a$10$bP3diImJKSifc/BY1y4zZe14vkCdhCs77YAlaM9JmWGhZzJFdZ3zi',
+        description: '',
+        profilePicURL: '',
+        imageID: '',
+    },
+
+    {
+        _id: ObjectId(`65be5813dad7a9ce85209436`),
+        username: 'user2',
+        password: '$2a$10$bP3diImJKSifc/BY1y4zZe14vkCdhCs77YAlaM9JmWGhZzJFdZ3zi',
+        description: '',
+        profilePicURL: '',
+        imageID: '',
+    },
+
+    {
+        _id: ObjectId(`65be5813dad7a9ce85209436`),
+        username: 'user3',
+        password: '$2a$10$bP3diImJKSifc/BY1y4zZe14vkCdhCs77YAlaM9JmWGhZzJFdZ3zi',
+        description: '',
+        profilePicURL: '',
+        imageID: '',
+    },
+
+    {
+        _id: ObjectId(`65be5813dad7a9ce85209436`),
+        username: 'user4',
+        password: '$2a$10$bP3diImJKSifc/BY1y4zZe14vkCdhCs77YAlaM9JmWGhZzJFdZ3zi',
+        description: '',
+        profilePicURL: '',
+        imageID: '',
+    },
+
 ]
