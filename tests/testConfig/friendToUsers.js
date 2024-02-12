92de4c5df7ac2fe4383ce25ed608c7cda847d281
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-02-12 14:56:16 +0000

    Create FriendToUsers TestConfig File

---
+++ tests/testConfig/friendToUsers.js
@@ -0,0 +1,28 @@
+import objectIds from './objectIds';
+
+const friendToUsers = [
+    {
+        _id: objectIds.friendToUserIDs[0],
+        user: objectIds.userIDs[0],
+        friendUser: objectIds.userIDs[1],
+    },
+
+    {
+        _id: objectIds.friendToUserIDs[1],
+        user: objectIds.userIDs[1],
+        friendUser: objectIds.userIDs[0],
+    },
+
+    {
+        _id: objectIds.friendToUserIDs[2],
+        user: objectIds.userIDs[0],
+        friendUser: objectIds.userIDs[2],
+    },
+
+    {
+        _id: objectIds.friendToUserIDs[3],
+        user: objectIds.userIDs[2],
+        friendUser: objectIds.userIDs[0],
+    },
+
+]
