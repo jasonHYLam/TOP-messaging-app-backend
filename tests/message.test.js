da85f4d8eafe64c84afacb796644822cf0af895b
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-03-05 09:28:47 +0000

    Setup All Message Tests

--- tests/message.test.js
+++ tests/message.test.js
@@ -21,3 +21,52 @@ beforeEach(async () => {
 afterEach(async () => {
   await dropDatabase();
 });
+
+describe("message tests", () => {
+  describe("get messages", () => {
+    it("gets all messages for a chat", async () => {})
+  })
+
+  describe("update message", () => {
+    it("updates message after successful edit", async () => {
+    })
+
+    it("does not update message after unsuccessful edit", async () => {
+    })
+
+  })
+
+  describe("create message", () => {
+    it("creates new message without image if successful validation", async () => {
+    })
+
+    it("does not create new message without image if unsuccessful validation", async () => {
+    })
+
+    it("creates new message with image if successful validation", async () => {
+    })
+
+    it("does not create new message with image if unsuccessful validation", async () => {
+    })
+
+    it("creates new reply without image if successful validation", async () => {
+    })
+
+    it("does not create new reply without image if unsuccessful validation", async () => {
+    })
+
+    it("creates new reply with image if successful validation", async () => {
+    })
+
+    it("does not create new reply with image if unsuccessful validation", async () => {
+    })
+
+  })
+
+  describe("delete message", () => {
+    it("successfully deletes message", async () => {
+
+    })
+
+  })
+})
