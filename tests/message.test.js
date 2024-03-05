782ae7aa79e215f8d2d9bb137944e1102ae93192
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-03-05 13:26:37 +0000

    Put Login In BeforeEach Hook For MessageTests

--- tests/message.test.js
+++ tests/message.test.js
@@ -5,8 +5,11 @@ const {initializeMongoServer, closeMongoServer, dropDatabase} = require("../mong
 const populateTestDB = require("./testConfig/populateTestDB");
 
 const messages = require("./testConfig/messages");
+const messageIds = messages.map((message) => message._id.toString());
 const chats = require("./testConfig/chats");
 const chatIds = chats.map((chat) => chat._id.toString());
+
+let agent;
 
 beforeAll(async () => {
   await initializeMongoServer();
@@ -18,6 +21,13 @@ afterAll(async () => {
 
 beforeEach(async () => {
   await populateTestDB();
+
+  agent = request.agent(app);
+  const loginResponse = await agent
+  .post("/login")
+  .send(loginData)
+  expect(loginResponse.status).toEqual(200);
+
 });
 
 afterEach(async () => {
@@ -29,32 +39,32 @@ const loginData = { username: "user1", password: "a" };
 describe("message tests", () => {
   describe("get messages", () => {
     it("gets all messages for a chat", async () => {
-      const agent = request.agent(app);
-      const loginResponse = await agent
-      .post("/login")
-      .send(loginData)
-      expect(loginResponse.status).toEqual(200);
 
       const getMessagesResponse = await agent
       .get(`/home/chat/${chatIds}`)
       expect(getMessagesResponse.status).toEqual(200);
 
       const returnedBody = getMessagesResponse.body
-      console.log('checking returnedBody')
-      console.log(returnedBody)
       const chatMessages = returnedBody.chat.chatMessages.map(message => message.text)
       expect(chatMessages).toEqual([
         messages[0].text,
         messages[1].text,
         messages[2].text,
       ])
-
     })
 
   })
 
   describe.skip("update message", () => {
     it("updates message after successful edit", async () => {
+      // const agent = request.agent(app);
+      // const loginResponse = await agent
+      // .post("/login")
+      // .send(loginData)
+      // expect(loginResponse.status).toEqual(200);
+
+      const editMessageResponse = agent
+      .put(`/home/chat/${chatIds[0]}/${messageIds[0]}`)
     })
 
     it("does not update message after unsuccessful edit", async () => {
