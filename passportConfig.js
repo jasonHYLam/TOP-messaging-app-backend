cc6eb4f00281e7f625fe4de1d9446c8a4d8e7321
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-03-19 16:18:05 +0000

    Remove Comments From PassportConfig

--- passportConfig.js
+++ passportConfig.js
@@ -11,30 +11,18 @@ function initializePassport(passport) {
         new LocalStrategy( async (username, password, done) => {
             try {
                 const user = await User.findOne({ username: username })
-                // console.log('seeking user')
                 if (!user) {
-                    // what happens after this
-                    console.log('checking username... ')
-                    console.log(username)
                     return done(null, false, { message: 'Incorrect username '})
                 }
 
                 const match = await bcrypt.compare(password, user.password)
-                // console.log('check if match is true')
-                // console.log(match)
                 if (!match) {
-                    console.log('no match ?!?!?!?')
-                    console.log('checking username and password!!!')
-                    console.log(username)
-                    console.log(password)
                     return done(null, false, { message: 'Incorrect password'})
                 }
 
-                // console.log('please work')
                 return done(null, user);
             }
             catch(err) {
-                // console.log('a senior moment perhaps')
                 return done(err)
             }
 
@@ -42,16 +30,10 @@ function initializePassport(passport) {
     )
 
     passport.serializeUser((user, done) => {
-        // console.log('serializeUser')
-        // console.log(user)
-        // console.log(user.id)
         done(null, user.id);
     })
 
     passport.deserializeUser(async (id, done) => {
-        // console.log('deserializeUser')
-        // console.log('what is id?')
-        // console.log(id)
         try {
             const user = await User.findById(id)
             done(null, user)
@@ -59,7 +41,6 @@ function initializePassport(passport) {
             done(err)
         } 
     })
-
 }
 
 module.exports = initializePassport;
