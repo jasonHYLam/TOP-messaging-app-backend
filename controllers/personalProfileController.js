a7052ad5f46152530c0717ba97f98351f6d664af
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-03-21 15:25:13 +0000

    Remove Comments From personalProfileController

--- controllers/personalProfileController.js
+++ controllers/personalProfileController.js
@@ -1,59 +1,40 @@
 const asyncHandler = require("express-async-handler");
+const { upload } = require("../cloudinaryConfig");
+const { body } = require("express-validator");
+const he = require("he");
 
-const { upload } = require('../cloudinaryConfig')
-
-
-
-const { body } = require('express-validator');
-const he = require('he');
-
-const User = require('../models/user');
+const User = require("../models/user");
 
 // Will not bother with changing username and password.
-// Regarding changing username, not sure how to deal with that logic.
 
 exports.get_logged_in_user = asyncHandler(async (req, res, next) => {
-     const loggedInUser = await User.findById(req.user.id, "username profilePicURL")
-     res.json({loggedInUser})
-})
+  const loggedInUser = await User.findById(
+    req.user.id,
+    "username profilePicURL"
+  );
+  res.json({ loggedInUser });
+});
 
 exports.change_description = [
-    body('changeToSubmit').trim().escape(),
+  body("changeToSubmit").trim().escape(),
 
-    asyncHandler(async (req, res, next) => {
-        // should get userid from req.user.id
-        console.log('checking changeToSubmit')
-        console.log(req.body)
-
-        await User.findByIdAndUpdate(req.user.id, {
-            description: he.decode(req.body.changeToSubmit)
-        })
-
-        res.json({})
-    })
-] 
+  asyncHandler(async (req, res, next) => {
+    await User.findByIdAndUpdate(req.user.id, {
+      description: he.decode(req.body.changeToSubmit),
+    });
+    res.json({});
+  }),
+];
 
 exports.change_image = [
+  upload.single("profilePic"),
 
-    upload.single('profilePic'),
-
-    asyncHandler(async (req, res, next) => {
-
-        // I have no idea how this works.
-        // upload.single('profilePic')
-        console.log('checking change_image')
-        console.log('req.file:')
-        console.log(req.file)
-
-        // modify User document.
-        await User.findByIdAndUpdate(req.user.id, {
-            profilePicURL: req.file.path,
-            imageID: req.file.fieldname
-        })
-        // need to get image URL, can i get it from req.file? or how do I access it through Cloudinary?
-
-        // res.json(req.file)
-        res.json({})
-
-    })
-] 
+  asyncHandler(async (req, res, next) => {
+    // modify User document.
+    await User.findByIdAndUpdate(req.user.id, {
+      profilePicURL: req.file.path,
+      imageID: req.file.fieldname,
+    });
+    res.json({});
+  }),
+];
